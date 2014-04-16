c0d69f6e6eb147af70740ad3df54f2a57c5402ac
Glidder <faceless.goblin@gmail.com>
2014-04-16 17:54:51 +0200

    various

---
+++ coraline/Animation.java
@@ -0,0 +1,21 @@
+/**
+* Class that supports the representation of animations.
+*
+* @author Luis José Quintana Bolaño (@Glidder_)
+* @version d1-14.3.6
+**/
+
+package coraline;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.logging.*;
+import javax.imageio.*;
+
+public class Animation {
+
+	
+}
